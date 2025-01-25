74386d6558ffd24c18ccba2fc5e7603545038bbd
Ishaan Vashishth <ishaan.vashishth@csdevents.com>
2025-01-25 19:14:18 +0700

    Save changes to assembly info files

--- obj/Debug/net8.0/Vashishth_Backened.24.AssemblyInfo.cs
+++ obj/Debug/net8.0/Vashishth_Backened.24.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vashishth_Backened.24")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ff62b5e56a32e4e150adb73fae79d043e1b840")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029e1bbaab7657d8e4fa72416ccb022302e1c4ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vashishth_Backened.24")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vashishth_Backened.24")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
